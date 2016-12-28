34bb12392f3b69ae692b60eb1fc8c2f1a0e26ea9
makinux <makinux7@gmail.com>
2016-12-28 10:15:33 +0900

    Acceleration using numpy

--- CSMap.py
+++ CSMap.py
@@ -1,100 +1,79 @@
 from PIL import Image
 import numpy as np
 import math
+import time
 
 class CSMap:
     def __init__(self, dem, unit, image_size=[256, 256]):
-        self.dem = np.array(dem)
         self.unit = float(unit)
         self.image_size_x = image_size[0]
         self.image_size_y = image_size[1]
+        if not isinstance(dem, np.ndarray):
+            dem = np.array(dem)
+        dem = np.r_[dem[0].reshape(1, len(dem[0])), dem]
+        dem = np.r_[dem, dem[self.image_size_y].reshape(1, len(dem[self.image_size_y]))]
+        dem = np.c_[dem[:,0].reshape(len(dem[:,0]), 1), dem]
+        dem = np.c_[dem, dem[:,self.image_size_x].reshape(len(dem[:,self.image_size_x]), 1)]
+        self.dem = dem
 
-        self.ticker = []
-        for i in range(0, 256):
-            self.ticker.append(math.pow(math.tan((math.pi / 2.0) * (i / 256.0)), 2))
-
-    def _slope(self, i, j):
-        if (i == 0):
-            return self._slope(i + 1, j)
-        if (i == 255):
-            return self._slope(i - 1, j)
-        if (j == 0):
-            return self._slope(i, j + 1)
-        if (j == 255):
-            return self._slope(i, j - 1)
+    def _slope(self):
 
         dem = self.dem / self.unit
 
-        y = (dem[i + 1][j] * 2 + dem[i + 1][j + 1] + dem[i + 1][j - 1]) - (dem[i - 1][j] * 2 + dem[i - 1][j - 1] + dem[i - 1][j + 1])
-        x = (dem[i][j + 1] * 2 + dem[i - 1][j + 1] + dem[i + 1][j + 1]) - (dem[i][j - 1] * 2 + dem[i - 1][j - 1] + dem[i + 1][j - 1])
+        y = (dem[2:, 1:-1] * 2.0 + dem[2:, 2:] + dem[2:, :-2]) - (dem[:-2, 1:-1] * 2.0 + dem[:-2, :-2] + dem[:-2, 2:])
+        x = (dem[1:-1, 2:] * 2.0 + dem[:-2, 2:] + dem[2:, 2:]) - (dem[1:-1, :-2] * 2.0 + dem[:-2, :-2] + dem[2:, :-2])
+
         z = x * x + y * y
 
-        for k, item in enumerate(self.ticker):
-            if item >= z:
-                return k
-        return 0
+        re_slope = (np.arctan(np.sqrt(z)) / math.pi) * 2.0 * 256.0
+        return re_slope
 
-
-    def _curvature(self, i, j):
+    def _curvature(self):
         dem = self.dem * 255.0 / self.unit
-        dem = np.r_[dem[0].reshape(1, len(dem[0])), dem]
-        dem = np.r_[dem, dem[255].reshape(1, len(dem[255]))]
-        dem = np.c_[dem[:,0].reshape(len(dem[:,0]), 1), dem]
-        dem = np.c_[dem, dem[:,255].reshape(len(dem[:,255]))]
 
-        i += 1
-        j += 1
-        c = 127
-        c += dem[i][j] * 4
-        c -= dem[i - 1][j]
-        c -= dem[i + 1][j]
-        c -= dem[i][j - 1]
-        c -= dem[i][j + 1]
+        c = np.zeros((self.image_size_y, self.image_size_x)) + 127
+        c += dem[1:-1, 1:-1] * 4
+        c -= dem[:-2, 1:-1]
+        c -= dem[2:, 1:-1]
+        c -= dem[1:-1, :-2]
+        c -= dem[1:-1, 2:]
 
         return c
 
 
     def cs_draw(self):
+        startall_time = time.time()
         slope_img = Image.new('RGBA', (self.image_size_x, self.image_size_y), (0, 0, 0, 0))
         curvature_img = Image.new('RGBA', (self.image_size_x, self.image_size_y), (0, 0, 0, 0))
+        start_time = time.time()
+        c1_list = self._slope()
+        #print 'Slope : ' + str(time.time() - start_time) + 'sec'
+        start_time = time.time()
+        c2_list = self._curvature()
+        #print 'Curvature : ' + str(time.time() - start_time) + 'sec'
+        start_time = time.time()
 
-        for i in range(0, self.image_size_y):
-            for j in range(0, self.image_size_x):
-                c1 = self._slope(i, j)
-                if (c1 < 160):
-                    t = c1 / 160.0
-                    r = int(255 + (255 - 255) * t)
-                    g = int(255 + (51 - 255) * t)
-                    b = int(255 + (0 - 255) * t)
-                    a = int(255 * 0.5)
-                    slope_img.putpixel((j,i),(r,g,b,a))
-                else:
-                    t = (c1 - 160.0) / (255.0 - 160.0)
-                    r = int(255 + (51 - 255) * t)
-                    g = int(51 + (0 - 51) * t)
-                    b = int(0 + (0 - 0) * t)
-                    a = int(255 * 0.5)
-                    slope_img.putpixel((j,i),(r,g,b,a))
+        slope_array = np.zeros((self.image_size_y, self.image_size_x, 4))
+        curvature_array = np.zeros((self.image_size_y, self.image_size_x, 4))
 
-                c2 = self._curvature(i, j)
-                if (c2 < 128):
-                    t = c2 / 128.0
-                    r = int(0 + (68 - 0) * t)
-                    g = int(0 + (68 - 0) * t)
-                    b = int(0 + (255 - 0) * t)
-                    a = 255
-                    curvature_img.putpixel((j,i),(r,g,b,a))
-                else:
-                    t = (c2 - 128.0) / (255.0 - 128.0)
-                    r = int(68 + (255 - 68) * t)
-                    g = int(68 + (255 - 68) * t)
-                    b = int(255 + (255 - 255) * t)
-                    a = 255
-                    curvature_img.putpixel((j,i),(r,g,b,a))
+        slope_array[:, :, 0] = np.where(c1_list < 160.0, (255 + (255 - 255) * (c1_list / 160.0)), (255 + (51 - 255) * ((c1_list - 160.0) / (255.0 - 160.0))))
+        slope_array[:, :, 1] = np.where(c1_list < 160.0, (255 + (51 - 255) * (c1_list / 160.0)), (51 + (0 - 51) * ((c1_list - 160.0) / (255.0 - 160.0))))
+        slope_array[:, :, 2] = np.where(c1_list < 160.0, (255 + (0 - 255) * (c1_list / 160.0)), (0 + (0 - 0) * ((c1_list - 160.0) / (255.0 - 160.0))))
+        slope_array[:, :, 3] = 255 * 0.5
+        slope_array = np.where(slope_array < 0, 0, slope_array)
+        slope_array = np.where(slope_array > 255, 255, slope_array)
+
+        curvature_array[:, :, 0] = np.where(c2_list < 128.0, (0 + (68 - 0) * (c2_list / 128.0)), (68 + (255 - 68) * ((c2_list - 128.0) / (255.0 - 128.0))))
+        curvature_array[:, :, 1] = np.where(c2_list < 128.0, (0 + (68 - 0) * (c2_list / 128.0)), (68 + (255 - 68) * ((c2_list - 128.0) / (255.0 - 128.0))))
+        curvature_array[:, :, 2] = np.where(c2_list < 128.0, (0 + (255 - 0) * (c2_list / 128.0)), (255 + (255 - 255) * ((c2_list - 128.0) / (255.0 - 128.0))))
+        curvature_array[:, :, 3] = 255
+        curvature_array = np.where(curvature_array < 0, 0, curvature_array)
+        curvature_array = np.where(curvature_array > 255, 255, curvature_array)
+
+        slope_img = Image.fromarray(np.uint8(slope_array))
+        curvature_img = Image.fromarray(np.uint8(curvature_array))
 
         cs_img = Image.alpha_composite(curvature_img, slope_img)
-        #slope_img.save('./slope_img.png')
-        #curvature_img.save('./curvature_img.png')
         return cs_img
 
 
